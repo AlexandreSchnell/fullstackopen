95628f661c5c74964593afe08833c3688e8b6730
Alexandre Schnell <aleschnell19@@gmail.com>
2025-06-21 15:03:56 -0300

    Exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,14 @@ const Title = ({ text }) => {
   )
 }
 
-const StatisticsLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = (props) => {
   if (props.all === 0) {
@@ -16,14 +23,16 @@ const Statistics = (props) => {
   }
 
   return (
-    <div>
-      <StatisticsLine text='good' value={props.good} />
-      <StatisticsLine text='neutral' value={props.neutral} />
-      <StatisticsLine text='bad' value={props.bad} />
-      <StatisticsLine text='all' value={props.all} />
-      <StatisticsLine text='average' value={props.average} />
-      <StatisticsLine text='positive' value={props.positive} />
-    </div>
+    <table>
+      <tbody>
+          <StatisticsLine text='good' value={props.good} />
+          <StatisticsLine text='neutral' value={props.neutral} />
+          <StatisticsLine text='bad' value={props.bad} />
+          <StatisticsLine text='all' value={props.all} />
+          <StatisticsLine text='average' value={props.average} />
+          <StatisticsLine text='positive' value={props.positive} />
+      </tbody>
+    </table>
   )
 }
 
